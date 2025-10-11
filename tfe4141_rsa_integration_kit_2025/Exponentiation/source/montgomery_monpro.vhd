a2e26bae9b0d34204e8f909a8e5f06757f175d69
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-10-11 18:49:56 +0200

    Added state machine to monpro and added testbench

--- tfe4141_rsa_integration_kit_2025/Exponentiation/source/montgomery_monpro.vhd
+++ tfe4141_rsa_integration_kit_2025/Exponentiation/source/montgomery_monpro.vhd
@@ -11,6 +11,13 @@ entity montgomery_monpro is
         clk : in std_logic;
         rst_n : in std_logic;
     --------------------------------------------------
+    -- Control signals
+    --------------------------------------------------
+        in_valid : in std_logic;
+        in_ready : out std_logic;
+        out_valid : out std_logic;
+        out_ready : in std_logic;
+    --------------------------------------------------
     -- Values to be multiplicated
     --------------------------------------------------
         a : in std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
@@ -28,39 +35,108 @@ end entity montgomery_monpro;
 
 architecture rtl of montgomery_monpro is
 
+    constant C_NUM_CALC_CYCLES : natural := 4;
+    signal s_calc_counter : unsigned(7 downto 0);
+
+    -- Buffered signals for input Values
+    signal s_a : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
+    signal s_b : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
+
     signal t : unsigned(GC_DATA_WIDTH * 2 - 1 downto 0);
     signal m : unsigned(GC_DATA_WIDTH - 1 downto 0);
     signal u_pre : unsigned(GC_DATA_WIDTH * 2 - 1 downto 0);
     signal u_shift : unsigned(GC_DATA_WIDTH - 1 downto 0);
 
+    type FSM is (ST_IDLE, ST_CALC, ST_HOLD);
+    signal state : FSM;
+
 begin
 
-    monpro_proc: process(clk)
+    monpro_proc: process(clk, rst_n)
+
         variable v_m : unsigned(GC_DATA_WIDTH * 3 - 1 downto 0) := (others => '0');
+
+        variable v_out_valid : std_logic;
+        variable v_in_ready : std_logic;
+
     begin
 
-        if rising_edge(clk) then
-            if rst_n = '0' then
-                u <= (others => '0');
+        v_m := (others => '0');
 
-            else
+        v_out_valid := '0';
+        v_in_ready := '0';
 
-                t <= unsigned(a) * unsigned(b);
-                v_m := (t * unsigned(n_prime));
-                m <= v_m(GC_DATA_WIDTH - 1 downto 0);
-                u_pre <= (t + m * unsigned(n));
-                u_shift <= u_pre(u_pre'left downto GC_DATA_WIDTH);
+        if rst_n = '0' then
 
-                if u_shift >= unsigned(n) then
-                    u <= std_logic_vector(u_shift - unsigned(n));
-                else
-                    u <= std_logic_vector(u_shift);
-                end if;
+            s_calc_counter <= (others => '0');
 
-            end if;
-        end if;
+            u <= (others => '0');
 
-        
+        elsif rising_edge(clk) then
+
+            -- Finite State Machine
+            case state is
+
+                --------------------------------------------------
+                when ST_IDLE =>
+                --------------------------------------------------
+                    if in_valid = '1' then
+                        state <= ST_CALC;
+                        s_calc_counter <= (others => '0');
+
+                        s_a <= a;
+                        s_b <= b;
+                    else
+                        v_in_ready := '1';
+                    end if;
+
+                --------------------------------------------------
+                when ST_CALC =>
+                --------------------------------------------------
+
+                    -- t should be calculated on the first clock cycle
+                    t <= unsigned(s_a) * unsigned(s_b);
+
+                    -- m is calculated on the second clock cycle (dependent on t)
+                    v_m := (t * unsigned(n_prime));
+                    m <= v_m(GC_DATA_WIDTH - 1 downto 0);
+
+                    -- rest is calculated on the third clock-cycle (dependent on m and t)
+                    u_pre <= (t + m * unsigned(n));
+                    u_shift <= u_pre(u_pre'left downto GC_DATA_WIDTH);
+
+                    -- Output should be ready after 4 clock cycles
+                    if u_shift >= unsigned(n) then
+                        u <= std_logic_vector(u_shift - unsigned(n));
+                    else
+                        u <= std_logic_vector(u_shift);
+                    end if;
+
+                    if s_calc_counter >= C_NUM_CALC_CYCLES then
+                        state <= ST_HOLD;
+                    end if;
+
+                    s_calc_counter <= s_calc_counter + 1;
+
+                --------------------------------------------------
+                when ST_HOLD =>
+                --------------------------------------------------
+
+                    v_out_valid := '1';
+
+                    if out_ready = '1' then
+                        state <= ST_IDLE;
+                    end if;
+
+                --------------------------------------------------
+                when others => state <= ST_IDLE;
+                --------------------------------------------------
+            end case;
+            
+            out_valid <= v_out_valid;
+            in_ready <= v_in_ready;
+            
+        end if; -- rising_edge
     end process monpro_proc;
     
 end architecture rtl;
---
+++ tfe4141_rsa_integration_kit_2025/Exponentiation/testbench/montgomery_monpro.vhd
@@ -0,0 +1,86 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity montgomery_monpro_tb is
+end entity montgomery_monpro_tb;
+
+
+architecture tb of montgomery_monpro_tb is
+
+    constant C_DATA_WIDTH : integer := 16;
+    constant C_CLOCK_PERIOD : time := 10 ns;
+
+    signal clk : std_logic := '0';
+    signal rst_n : std_logic := '0';
+
+    signal in_valid : std_logic := '0';
+    signal in_ready : std_logic := '0';
+    signal out_valid : std_logic := '0';
+    signal out_ready : std_logic := '0';
+
+    signal a : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+    signal b : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+
+    signal n : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+    signal n_prime : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+
+    signal u : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+    
+begin
+    
+    DUT : entity work.montgomery_monpro
+        port map(
+            clk => clk,
+            rst_n => rst_n,
+            --------------------------------------------------
+            in_valid => in_valid,
+            in_ready => in_ready,
+            out_valid => out_valid,
+            out_ready => out_ready,
+            --------------------------------------------------
+            a => a,
+            b => b,
+            --------------------------------------------------
+            n => n,
+            n_prime => n_prime,
+            --------------------------------------------------
+            u => u
+        );
+
+    clk <= not clk after C_CLOCK_PERIOD / 2;
+
+    SEQUENCER_PROC: process
+    begin
+
+
+        rst_n <= '1' after C_CLOCK_PERIOD;
+
+        n <= std_logic_vector(to_unsigned(143, C_DATA_WIDTH)) after 3 * C_CLOCK_PERIOD;
+        n_prime <= std_logic_vector(to_unsigned(57745, C_DATA_WIDTH)) after 3 * C_CLOCK_PERIOD;
+
+        a <= std_logic_vector(to_unsigned(130, C_DATA_WIDTH)) after 3 * C_CLOCK_PERIOD;
+        b <= std_logic_vector(to_unsigned(48, C_DATA_WIDTH)) after 3 * C_CLOCK_PERIOD;
+
+        in_valid <= '1' after 4 * C_CLOCK_PERIOD;
+
+        out_ready <= '1' after 10 * C_CLOCK_PERIOD;
+        
+        wait for 10 * C_CLOCK_PERIOD;
+        assert u /= std_logic_vector(to_unsigned(26, C_DATA_WIDTH)) report "Incorrect result" severity note;
+        report "Testbench finished";
+
+    end process SEQUENCER_PROC;
+
+
+    stage_1_proc: process(clk, rst_n)
+    begin
+        if rst_n = '0' then
+            
+        elsif rising_edge(clk) then
+            
+        end if;
+    end process stage_1_proc;
+    
+end architecture tb;
