453b6edd1fca24fe33736c95c149322b1013d50f
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-10-28 13:07:59 +0100

    Working on implementing montgomery_monpro_cios in vhdl

--- tfe4141_rsa_integration_kit_2025/Exponentiation/source/montgomery_monpro_cios.vhd
+++ tfe4141_rsa_integration_kit_2025/Exponentiation/source/montgomery_monpro_cios.vhd
@@ -43,14 +43,17 @@ architecture rtl of montgomery_monpro_cios is
     signal s_a : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
     signal s_b : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
     signal s_n : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
-    signal s_n_prime : std_logic_vector(GC_DATA_WIDTH - 1 downto 0);
-    --
+    signal s_n_prime : std_logic_vector(GC_LIMB_WIDTH - 1 downto 0); -- for CIOS we only need the first part of n_prime
+    
     -- signal t : unsigned(GC_DATA_WIDTH * 2 - 1 downto 0);
     -- signal m : unsigned(GC_DATA_WIDTH - 1 downto 0);
     -- signal u_shift : unsigned(GC_DATA_WIDTH - 1 downto 0);
 
-    type FSM is (ST_IDLE, ST_CALC, ST_HOLD);
-    signal state : FSM;
+    type T_FSM is (ST_IDLE, ST_CALC, ST_HOLD);
+    signal state : T_FSM;
+
+    type T_ARRAY is array(0 to GC_NUM_LIMBS) of std_logic_vector(GC_LIMB_WIDTH - 1 downto 0);
+    signal input_limbs : T_ARRAY;
 
 begin
 
@@ -58,8 +61,8 @@ begin
 
         -- variable v_m : unsigned(GC_DATA_WIDTH * 3 - 1 downto 0) := (others => '0');
         --
-        -- variable v_out_valid : std_logic;
-        -- variable v_in_ready : std_logic;
+        variable v_out_valid : std_logic;
+        variable v_in_ready : std_logic;
         --
         -- variable v_u_pre : unsigned(GC_DATA_WIDTH * 2 - 1 downto 0) := (others => '0');
 
@@ -67,9 +70,9 @@ begin
 
         -- v_m := (others => '0');
 
-        -- v_out_valid := '0';
-        -- v_in_ready := '0';
-        --
+        v_out_valid := '0';
+        v_in_ready := '0';
+ 
         -- v_u_pre := (others => '0');
 
         if rst_n = '0' then
@@ -88,14 +91,15 @@ begin
                 when ST_IDLE =>
                 --------------------------------------------------
                     if in_valid = '1' then
-                        state <= ST_CALC;
                         -- s_calc_counter <= (others => '0');
 
+                        -- Load input to registers
                         s_a <= a;
                         s_b <= b;
                         s_n <= n;
                         s_n_prime <= n_prime;
 
+                        state <= ST_CALC;
                     else
                         v_in_ready := '1';
                     end if;
@@ -105,28 +109,28 @@ begin
                 --------------------------------------------------
 
                     -- t should be calculated on the first clock cycle
-                    t <= unsigned(s_a) * unsigned(s_b);
+                    -- t <= unsigned(s_a) * unsigned(s_b);
 
                     -- m is calculated on the second clock cycle (dependent on t)
-                    v_m := (t * unsigned(n_prime));
-                    m <= v_m(GC_DATA_WIDTH - 1 downto 0);
+                    -- v_m := (t * unsigned(n_prime));
+                    -- m <= v_m(GC_DATA_WIDTH - 1 downto 0);
 
                     -- rest is calculated on the third clock-cycle (dependent on m and t)
-                    v_u_pre := (t + m * unsigned(n));
-                    u_shift <= v_u_pre(v_u_pre'left downto GC_DATA_WIDTH);
+                    -- v_u_pre := (t + m * unsigned(n));
+                    -- u_shift <= v_u_pre(v_u_pre'left downto GC_DATA_WIDTH);
 
                     -- Output should be ready after 4 clock cycles
-                    if u_shift >= unsigned(n) then
-                        u <= std_logic_vector(u_shift - unsigned(n));
-                    else
-                        u <= std_logic_vector(u_shift);
-                    end if;
+                    -- -- if u_shift >= unsigned(n) then
+                    --     u <= std_logic_vector(u_shift - unsigned(n));
+                    -- else
+                    --     u <= std_logic_vector(u_shift);
+                    -- end if;
+                    --
+                    -- if s_calc_counter >= C_NUM_CALC_CYCLES then
+                    --     state <= ST_HOLD;
+                    -- end if;
 
-                    if s_calc_counter >= C_NUM_CALC_CYCLES then
-                        state <= ST_HOLD;
-                    end if;
-
-                    s_calc_counter <= s_calc_counter + 1;
+                    -- s_calc_counter <= s_calc_counter + 1;
 
                 --------------------------------------------------
                 when ST_HOLD =>
