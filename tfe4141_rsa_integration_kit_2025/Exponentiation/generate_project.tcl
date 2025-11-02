3962798d51e54c791d448d34c58fae3eb5a4c197
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-11-02 15:53:29 +0100

    Updated tcl script to generate project

--- tfe4141_rsa_integration_kit_2025/Exponentiation/generate_project.tcl
+++ tfe4141_rsa_integration_kit_2025/Exponentiation/generate_project.tcl
@@ -17,12 +17,14 @@ set IP_directory ""
 #source for to be included in synthesis
 set source_files [list \
 	{*}[glob -nocomplain -directory [file normalize "$origin_dir/source/"] -type f *] \
+	{*}[glob -nocomplain -directory [file normalize "$origin_dir/source/processing_elements/"] -type f *] \
 	{*}[include_from_file $origin_dir [file normalize "$origin_dir/include.txt"]] \
 ]
 
 #source file to be only included in simulation
 set sim_files [list \
 	{*}[glob -nocomplain -directory [file normalize "$origin_dir/testbench/"] -type f *]\
+	{*}[glob -nocomplain -directory [file normalize "$origin_dir/testbench/processing_elements/"] -type f *]\
 ]
 
 genProj $_xil_proj_name_ $top_design $top_design_testbench $source_files $sim_files $IP_directory
