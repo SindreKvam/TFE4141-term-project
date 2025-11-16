b7fa06f5a8372f885671b00aeb87ca3efda34b90
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-11-16 22:52:30 +0100

    Formatted and updated where from_limbs is performed on montgomery monpro systolic array implementatioN

--- high-level-simulation/python/src/montgomery_monpro_cios_systolic_array.py
+++ high-level-simulation/python/src/montgomery_monpro_cios_systolic_array.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 
 from generate_rsa_key_values import RsaKeyValues, get_rsa_key_values
-from montgomery_monpro_cios import carry_sum, to_limbs, from_limbs
+from montgomery_monpro_cios import carry_sum, from_limbs, to_limbs
 
 logger = logging.getLogger(__name__)
 
@@ -83,7 +83,7 @@ def montgomery_monpro_cios_systolic_array(a, b, w, s, n, n_prime):
         T_int = int(from_limbs(T, w)) - from_limbs(n, w)
         T = to_limbs(T_int, s, w)
 
-    return T
+    return from_limbs(T, w)
 
 
 def montgomery_monpro_cios_systolic_array_nw_16_verbose(a, b, w, s, n, n_prime):
@@ -109,42 +109,42 @@ def montgomery_monpro_cios_systolic_array_nw_16_verbose(a, b, w, s, n, n_prime):
     for i in range(s):
         C = 0
 
-        C, T[0] = alpha(a[0], b[i], T[0], C, w=w) # alpha 1
-        C, T[1] = alpha(a[1], b[i], T[1], C, w=w) # alpha 1
-        C, T[2] = alpha(a[2], b[i], T[2], C, w=w) # alpha 1
-        C, T[3] = alpha(a[3], b[i], T[3], C, w=w) # alpha 2
-        C, T[4] = alpha(a[4], b[i], T[4], C, w=w) # alpha 2
-        C, T[5] = alpha(a[5], b[i], T[5], C, w=w) # alpha 2
-        C, T[6] = alpha(a[6], b[i], T[6], C, w=w) # alpha 3
-        C, T[7] = alpha(a[7], b[i], T[7], C, w=w) # alpha 3
-        C, T[8] = alpha(a[8], b[i], T[8], C, w=w) # alpha 3
-        C, T[9] = alpha(a[9], b[i], T[9], C, w=w) # alpha 4
-        C, T[10] = alpha(a[10], b[i], T[10], C, w=w) # alpha 4
-        C, T[11] = alpha(a[11], b[i], T[11], C, w=w) # alpha 4
-        C, T[12] = alpha(a[12], b[i], T[12], C, w=w) # alpha 5
-        C, T[13] = alpha(a[13], b[i], T[13], C, w=w) # alpha 5
-        C, T[14] = alpha(a[14], b[i], T[14], C, w=w) # alpha 5
-        C, T[15] = alpha(a[15], b[i], T[15], C, w=w) # alpha 6
+        C, T[0] = alpha(a[0], b[i], T[0], C, w=w)  # alpha 1
+        C, T[1] = alpha(a[1], b[i], T[1], C, w=w)  # alpha 1
+        C, T[2] = alpha(a[2], b[i], T[2], C, w=w)  # alpha 1
+        C, T[3] = alpha(a[3], b[i], T[3], C, w=w)  # alpha 2
+        C, T[4] = alpha(a[4], b[i], T[4], C, w=w)  # alpha 2
+        C, T[5] = alpha(a[5], b[i], T[5], C, w=w)  # alpha 2
+        C, T[6] = alpha(a[6], b[i], T[6], C, w=w)  # alpha 3
+        C, T[7] = alpha(a[7], b[i], T[7], C, w=w)  # alpha 3
+        C, T[8] = alpha(a[8], b[i], T[8], C, w=w)  # alpha 3
+        C, T[9] = alpha(a[9], b[i], T[9], C, w=w)  # alpha 4
+        C, T[10] = alpha(a[10], b[i], T[10], C, w=w)  # alpha 4
+        C, T[11] = alpha(a[11], b[i], T[11], C, w=w)  # alpha 4
+        C, T[12] = alpha(a[12], b[i], T[12], C, w=w)  # alpha 5
+        C, T[13] = alpha(a[13], b[i], T[13], C, w=w)  # alpha 5
+        C, T[14] = alpha(a[14], b[i], T[14], C, w=w)  # alpha 5
+        C, T[15] = alpha(a[15], b[i], T[15], C, w=w)  # alpha 6
 
         T[16 + 1], T[16] = alpha_final(C, T[16], w=w)
 
         C, m = beta(T[0], n[0], n_prime[0], w=w)
 
-        C, T[1 - 1] = gamma(n[1], m, C, T[1], w=w) # gamma 1
-        C, T[2 - 1] = gamma(n[2], m, C, T[2], w=w) # gamma 2
-        C, T[3 - 1] = gamma(n[3], m, C, T[3], w=w) # gamma 2
-        C, T[4 - 1] = gamma(n[4], m, C, T[4], w=w) # gamma 2
-        C, T[5 - 1] = gamma(n[5], m, C, T[5], w=w) # gamma 3
-        C, T[6 - 1] = gamma(n[6], m, C, T[6], w=w) # gamma 3
-        C, T[7 - 1] = gamma(n[7], m, C, T[7], w=w) # gamma 3
-        C, T[8 - 1] = gamma(n[8], m, C, T[8], w=w) # gamma 4
-        C, T[9 - 1] = gamma(n[9], m, C, T[9], w=w) # gamma 4
-        C, T[10 - 1] = gamma(n[10], m, C, T[10], w=w) # gamma 4
-        C, T[11 - 1] = gamma(n[11], m, C, T[11], w=w) # gamma 5
-        C, T[12 - 1] = gamma(n[12], m, C, T[12], w=w) # gamma 5
-        C, T[13 - 1] = gamma(n[13], m, C, T[13], w=w) # gamma 5
-        C, T[14 - 1] = gamma(n[14], m, C, T[14], w=w) # gamma 6
-        C, T[15 - 1] = gamma(n[15], m, C, T[15], w=w) # gamma 6
+        C, T[1 - 1] = gamma(n[1], m, C, T[1], w=w)  # gamma 1
+        C, T[2 - 1] = gamma(n[2], m, C, T[2], w=w)  # gamma 2
+        C, T[3 - 1] = gamma(n[3], m, C, T[3], w=w)  # gamma 2
+        C, T[4 - 1] = gamma(n[4], m, C, T[4], w=w)  # gamma 2
+        C, T[5 - 1] = gamma(n[5], m, C, T[5], w=w)  # gamma 3
+        C, T[6 - 1] = gamma(n[6], m, C, T[6], w=w)  # gamma 3
+        C, T[7 - 1] = gamma(n[7], m, C, T[7], w=w)  # gamma 3
+        C, T[8 - 1] = gamma(n[8], m, C, T[8], w=w)  # gamma 4
+        C, T[9 - 1] = gamma(n[9], m, C, T[9], w=w)  # gamma 4
+        C, T[10 - 1] = gamma(n[10], m, C, T[10], w=w)  # gamma 4
+        C, T[11 - 1] = gamma(n[11], m, C, T[11], w=w)  # gamma 5
+        C, T[12 - 1] = gamma(n[12], m, C, T[12], w=w)  # gamma 5
+        C, T[13 - 1] = gamma(n[13], m, C, T[13], w=w)  # gamma 5
+        C, T[14 - 1] = gamma(n[14], m, C, T[14], w=w)  # gamma 6
+        C, T[15 - 1] = gamma(n[15], m, C, T[15], w=w)  # gamma 6
 
         T[16 - 1], T[16] = gamma_final(C, T[16], T[16 + 1], w=w)
 
@@ -163,39 +163,26 @@ def montgomery_modexp(M, e, n, w, s, key_values: RsaKeyValues):
 
     k = w * s
 
-    M_bar = from_limbs(
-        montgomery_monpro_cios_systolic_array(
-            M, key_values.r2_mod_n, w, s, n, key_values.n_0_prime
-        ),
-        w,
+    M_bar = montgomery_monpro_cios_systolic_array(
+        M, key_values.r2_mod_n, w, s, n, key_values.n_0_prime
     )
-    C_bar = from_limbs(
-        montgomery_monpro_cios_systolic_array(
-            1, key_values.r2_mod_n, w, s, n, key_values.n_0_prime
-        ),
-        w,
+    C_bar = montgomery_monpro_cios_systolic_array(
+        1, key_values.r2_mod_n, w, s, n, key_values.n_0_prime
     )
 
     binary_e = f"{e:b}".zfill(k)
     for bit in binary_e:
         bit = int(bit)
 
-        C_bar = from_limbs(
-            montgomery_monpro_cios_systolic_array(
-                C_bar, C_bar, w, s, n, key_values.n_0_prime
-            ),
-            w,
+        C_bar = montgomery_monpro_cios_systolic_array(
+            C_bar, C_bar, w, s, n, key_values.n_0_prime
         )
         if bit == 1:
-            C_bar = from_limbs(
-                montgomery_monpro_cios_systolic_array(
-                    M_bar, C_bar, w, s, n, key_values.n_0_prime
-                ),
-                w,
+            C_bar = montgomery_monpro_cios_systolic_array(
+                M_bar, C_bar, w, s, n, key_values.n_0_prime
             )
-    return from_limbs(
-        montgomery_monpro_cios_systolic_array(C_bar, 1, w, s, n, key_values.n_0_prime),
-        w,
+    return montgomery_monpro_cios_systolic_array(
+        C_bar, 1, w, s, n, key_values.n_0_prime
     )
 
 
